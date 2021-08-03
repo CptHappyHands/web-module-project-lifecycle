cf53cc918709ca69bd777c71e6474a4e22c4ebb9
Andrew Cummings <andrew.amalee@gmail.com>
2021-08-03 14:53:40 -0700

    info connected and rendering

---
+++ myapp/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+  state = {
+    userData: [] 
+  }
+
+  render() {
+    return(
+      <div>
+        <h1>Git Hub User Cards</h1>
+        <form>
+          <input />
+          <button>Click here</button>
+        </form>
+      </div>
+    )
+  }
+
+
+}
+
+export default App;
